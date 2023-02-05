345a6f154ddbd7f7f4bf8e1bf9e03c963ed17ae9
Narek199114 <118613946+Narek199114@users.noreply.github.com>
2023-02-05 17:12:05 +0400

    Add files via upload
    Weather App

---
+++ app.js
@@ -0,0 +1,37 @@
+const input = document.querySelector("#input");
+const city = document.querySelector("#city");
+
+const cityName = document.querySelector("#cityName");
+const Temp = document.querySelector("#temp");
+const main = document.querySelector("#main");
+const discription = document.querySelector("#discription");
+const image = document.querySelector("#image");
+
+input.onsubmit = (e) => {
+  e.preventDefault();
+  weatherUpdate(city.value);
+  city.value = "";
+};
+
+weatherUpdate = (city) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cad7ec124945dcfff04e457e76760d90`);
+
+  xhr.send();
+  xhr.onload = () => {
+    if (xhr.status === 404) {
+      alert("Place not found");
+    } else {
+      let data = JSON.parse(xhr.response);
+      cityName.innerHTML = data.name;
+      Temp.innerHTML = `${Math.round(data.main.temp - 273.15)}°C`;
+      main.innerHTML = data.weather[0].main;
+      discription.innerHTML = data.weather[0].description;
+      image.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    }
+  };
+};
+
+weatherUpdate("Vanadzor");
